938cbb95333dfdac064979f71352fb91e3c49ef1
luvuyo_mazibuko <fisokuhlelavuyo97@gmail.com>
2024-02-28 15:20:26 +0200

    changes to chapter 4 project, and quiz

--- ch04/pr4.js
+++ ch04/pr4.js
@@ -1,23 +1,23 @@
 "use strict"
 // friend checker game
 
-let userName = prompt("what is your name?").toLowerCase();
+// let userName = prompt("what is your name?").toLowerCase();
 
-switch (userName) {
-    case "njabulo":
-    case "mmeli":
-    case "nokwanda":
-    case "ayanda":
-    case "ntokozo":
-        console.log("hello friend!");
-        break;
-    default:
-        console.log("hello stranger!")
-};
+// switch (userName) {
+//     case "njabulo":
+//     case "mmeli":
+//     case "nokwanda":
+//     case "ayanda":
+//     case "ntokozo":
+//         console.log("hello friend!");
+//         break;
+//     default:
+//         console.log("hello stranger!")
+// };
 
 // rock paper scissors game
 
-array1 = [
+let array1 = [
     "rock",
     "paper",
     "scissors"
@@ -29,13 +29,22 @@ let c2 = Math.floor(Math.random() * 3);
 let responsePlayer = array1[p1];
 let responseComputer = array1[c2];
 
-switch (responsePlayer) {
+switch (true) {
     case responsePlayer === responseComputer:
+        console.log(`player: ${responsePlayer}\ncomputer: ${responseComputer}`);
         console.log("its a tie");
         break;
-    case responsePlayer
-    
-}
+    case responsePlayer === array1[0] && responseComputer === array1[2]:
+    case responsePlayer === array1[1] && responseComputer === array1[0]:
+    case responsePlayer === array1[2] && responseComputer === array1[1]:
+        console.log(`player: ${responsePlayer}\ncomputer: ${responseComputer}`);
+        console.log("player wins");
+        break;
+    default:
+        console.log(`player: ${responsePlayer}\ncomputer: ${responseComputer}`);
+        console.log("computer wins");
+
+};
 
 
 
